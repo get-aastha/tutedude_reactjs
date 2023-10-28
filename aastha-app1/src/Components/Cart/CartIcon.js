b9e549d4a6cf74a7efba8be910b7179aff60305f
Aastha Kumar <108509128+get-aastha@users.noreply.github.com>
2023-10-28 18:45:18 +0530

    Add files via upload

---
+++ aastha-app1/src/Components/Cart/CartIcon.js
@@ -0,0 +1,15 @@
+const CartIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        d="M3 1a1 1 0 000 2h1.22.305 1.222a.997.997 0 00.01.042 11.358 5.43-.893.892 3.74 11.846 4.632 14 6.414"
+      ></path>
+    </svg>
+  );
+};
+
+export default CartIcon;
